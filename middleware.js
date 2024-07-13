d7bd596827981771682eb9611338304753f05d80
Vikashnaidu007 <vikashkirla007@gmail.com>
2024-07-13 16:47:02 -0400

    Fix typo in middleware.js

--- middleware.js
+++ middleware.js
@@ -43,7 +43,7 @@ module.exports.isAuthor = async(req, res, next) => {
 
 module.exports.isReviewAuthor = async(req, res, next) => {
     const {id, reviewId}= req.params;
-    const review= await Review.findById(reviewIdid);
+    const review= await Review.findById(reviewId);
     if(!review.author.equals(req.user._id)){
         req.flash('error', 'Permission Denied')
         res.redirect(`/campgrounds/${id}`)
